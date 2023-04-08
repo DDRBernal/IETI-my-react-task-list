1da5a38e1085c2dd22dd8fa6a5f7dd2eadcb54f2
David.O <46855679+DDRBernal@users.noreply.github.com>
2023-04-07 19:39:52 -0500

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,29 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function addTask(){
-  var task_name1 = document.getElementById("task_name");
-  var elemento = document.getElementById("task_menu");
-  //if (task_name1!=null){
-    elemento.innerHTML +=
-          `<div>
-          <input type = "checkbox"></input>
-          <label for="task1"> ${task_name1}  </label>
-          <button class="button"> <i>-</i></button>
-        </div>`
-  //} 
+function addTask() {
+  const taskName = document.getElementById("task_name").value.trim();
+  if (taskName) {
+    const taskMenu = document.getElementById("task_menu");
+    const newTask = document.createElement("div");
+    newTask.innerHTML = `
+      <div>
+        <input type="checkbox"></input>
+        <label>${taskName}</label>
+        <button class="button" onClick="removeTask(this)"> <i>-</i></button>
+      </div>
+    `;
+    taskMenu.appendChild(newTask);
+    document.getElementById("task_name").value = "";
+  }
+}
 
+function removeTask(button) {
+  const taskDiv = button.closest("div");
+  taskDiv.remove();
 }
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
       <div>
@@ -30,46 +36,36 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Task List</h1> 
-      <input type="text" name="task_name" placeholder='Add your new todo'></input>
+      <h1>Task List</h1>
+      <input type="text" id="task_name" placeholder='Add your new todo'></input>
       <form>
-      
-      <p>&nbsp;</p>
-
-  
-  <label for="Tasks"></label>
-
-  <div id="task_menu">
-    <div>
-      <input type = "checkbox"></input>
-      <label for="task1"> Buy a new gaming laptop  </label>
-      <button class="button"> <i>-</i></button>
-    </div>
-
-    <div>
-      <input type = "checkbox"></input>
-      <label for="task2"> Complete a previous task  </label>
-      <button class="button"> <i>-</i></button>
-    </div>
-
-    <div>
-      <input type = "checkbox"></input>
-      <label for="task3"> Create video for Youtube  </label>
-      <button class="button"> <i>-</i></button>
-    </div>
-
-    <div>
-      <input type = "checkbox"></input>
-      <label for="task4"> Create a new portafolio site </label>
-      <button class="button"> <i>-</i></button>
-    </div>
-  </div>
-  
-  
-</form>
+        <label htmlFor="Tasks"></label>
+        <div id="task_menu">
+          <div>
+            <input type="checkbox"></input>
+            <label>Buy a new gaming laptop</label>
+            <button class="button" onClick="removeTask(this)"> <i>-</i></button>
+          </div>
+          <div>
+            <input type="checkbox"></input>
+            <label>Complete a previous task</label>
+            <button class="button" onClick="removeTask(this)"> <i>-</i></button>
+          </div>
+          <div>
+            <input type="checkbox"></input>
+            <label>Create video for Youtube</label>
+            <button class="button" onClick="removeTask(this)"> <i>-</i></button>
+          </div>
+          <div>
+            <input type="checkbox"></input>
+            <label>Create a new portfolio site</label>
+            <button class="button" onClick="removeTask(this)"> <i>-</i></button>
+          </div>
+        </div>
+      </form>
       <div className="card">
         <button onClick={addTask}>
-          Add new task 
+          Add new task
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
